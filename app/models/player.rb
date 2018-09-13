e182a1d1a3b8882b77d38c3dc5a079329795db10
reeseglasscock <reesegpdx@gmail.com>
2018-09-13 11:49:22 -0700

    create after_create method to update basic stats with new user creation

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,4 +5,14 @@ class Player < ApplicationRecord
   has_many :armors, through: :inventories
   has_many :consumables, through: :inventories
   has_many :game_sessions
+  after_create :add_stats
+  validates :name, presence: true
+  validates :strength, presence: true
+  validates :endurance, presence: true
+  validates :constitution, presence: true
+  validates :intelligence, presence: true
+
+  def add_stats
+    self.update(health: 100, stamina: 100, shade: 100, level: 1, exp: 0)
+  end
 end
